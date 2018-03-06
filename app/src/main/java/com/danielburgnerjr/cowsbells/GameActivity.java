bb6b220d240229478a1e8a3534b643dce3526257
Daniel Burgner <dburgnerjr@yahoo.com>
2018-03-05 22:02:33 -0600

    D64 Code Cleanup/Set Answer function.
    D64 of #100DaysOfCode.  Performed code cleanup and created initial
    setAnswer function in #CowsBells app.  #BuiltByUdacity
    #GoogleUdacityScholars

--- app/src/main/java/com/danielburgnerjr/cowsbells/GameActivity.java
+++ app/src/main/java/com/danielburgnerjr/cowsbells/GameActivity.java
@@ -6,12 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Random;
+
 
 public class GameActivity extends Activity {
 	
 	private EditText etGuess;			// guess
+	private String strAnswer;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,21 +24,54 @@ public class GameActivity extends Activity {
 		nGameCode = Integer.parseInt(getIntent().getExtras().get("game_code").toString());
 		switch (nGameCode) {
             case 4:
-                Toast.makeText(getApplicationContext(), "Easy", Toast.LENGTH_SHORT).show();
+                strAnswer = setAnswer(nGameCode);
+                Toast.makeText(getApplicationContext(), "Easy" + strAnswer, Toast.LENGTH_SHORT).show();
                 break;
             case 5:
-                Toast.makeText(getApplicationContext(), "Medium", Toast.LENGTH_SHORT).show();
+                strAnswer = setAnswer(nGameCode);
+                Toast.makeText(getApplicationContext(), "Medium" + strAnswer, Toast.LENGTH_SHORT).show();
                 break;
             case 6:
-                Toast.makeText(getApplicationContext(), "Hard", Toast.LENGTH_SHORT).show();
+                strAnswer = setAnswer(nGameCode);
+                Toast.makeText(getApplicationContext(), "Hard" + strAnswer, Toast.LENGTH_SHORT).show();
                 break;
             case 7:
-                Toast.makeText(getApplicationContext(), "Super Hard", Toast.LENGTH_SHORT).show();
+                strAnswer = setAnswer(nGameCode);
+                Toast.makeText(getApplicationContext(), "Super Hard" + strAnswer, Toast.LENGTH_SHORT).show();
                 break;
         }
 		
 		etGuess  = (EditText)findViewById(R.id.txtGuess);
 	}
+
+	public String setAnswer(int nGC) {
+	    String strA = "";
+
+        Random rand = new Random();
+        int nDigit;
+        boolean bMatch = false;
+
+	    for (int nI = 0; nI < nGC; nI++) {
+	         nDigit = rand.nextInt(10);
+/*
+	         if (nI > 0) {
+                 for (int nJ = 1; nJ <= nI; nI++) {
+                     if (Integer.parseInt(strA.substring(nJ - 1, nJ)) == nDigit) {
+                         bMatch = true;
+                     }
+                 }
+             }
+             if (bMatch == false) {
+*/
+                 strA += nDigit;
+/*
+             } else {
+	             nI -= 1;
+             }
+*/
+        }
+        return strA;
+    }
 
 	public void submit(View view) {
 		String strSubmit = "Submit";
