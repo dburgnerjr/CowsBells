7aff2e0abfcbb9ae3bedb4b29e4f23f402a985e1
Daniel Burgner <dburgnerjr@yahoo.com>
2018-03-08 22:45:29 -0600

    D67 First attempt.
    D67 of #100DaysOfCode.  First attempt of compareAnswer function of
    #CowsBells app #AndroidDev #BuiltByUdacity #GoogleUdacityScholars

--- app/src/main/java/com/danielburgnerjr/cowsbells/GameActivity.java
+++ app/src/main/java/com/danielburgnerjr/cowsbells/GameActivity.java
@@ -8,42 +8,23 @@ import android.widget.EditText;
 import android.widget.Toast;
 import java.util.Collections;
 import java.util.ArrayList;
-import java.util.Random;
-
 
 public class GameActivity extends Activity {
 	
 	private EditText etGuess;			// guess
 	private String strAnswer;
+	private int nGameCode;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.game_activity);
 
-		int nGameCode;
-
 		Intent intI = getIntent();
 		nGameCode = Integer.parseInt(getIntent().getExtras().get("game_code").toString());
-		switch (nGameCode) {
-            case 4:
-                strAnswer = setAnswer(nGameCode);
-                Toast.makeText(getApplicationContext(), "Easy " + strAnswer, Toast.LENGTH_SHORT).show();
-                break;
-            case 5:
-                strAnswer = setAnswer(nGameCode);
-                Toast.makeText(getApplicationContext(), "Medium " + strAnswer, Toast.LENGTH_SHORT).show();
-                break;
-            case 6:
-                strAnswer = setAnswer(nGameCode);
-                Toast.makeText(getApplicationContext(), "Hard " + strAnswer, Toast.LENGTH_SHORT).show();
-                break;
-            case 7:
-                strAnswer = setAnswer(nGameCode);
-                Toast.makeText(getApplicationContext(), "Super Hard " + strAnswer, Toast.LENGTH_SHORT).show();
-                break;
-        }
-		
+		strAnswer = setAnswer(nGameCode);
+
 		etGuess  = (EditText)findViewById(R.id.txtGuess);
+        Toast.makeText(getApplicationContext(), strAnswer, Toast.LENGTH_SHORT).show();
 	}
 
 	public String setAnswer(int nGC) {
@@ -64,12 +45,32 @@ public class GameActivity extends Activity {
 	public void submit(View view) {
 		String strSubmit = "Submit";
 		String strGuess = etGuess.getText().toString();
-		Toast.makeText(getApplicationContext(), strSubmit + " " + strGuess, Toast.LENGTH_SHORT).show();	
+		Toast.makeText(getApplicationContext(), strSubmit + " " + strGuess, Toast.LENGTH_SHORT).show();
+		compareAnswer(strGuess);
 	}
 
 	public void newGame(View view) {
 		String strNewGame = "New Game";
-		Toast.makeText(getApplicationContext(), strNewGame, Toast.LENGTH_SHORT).show();	
+        strAnswer = setAnswer(nGameCode);
+		Toast.makeText(getApplicationContext(), strNewGame + " " + strAnswer, Toast.LENGTH_SHORT).show();
 	}
 
+	public void compareAnswer(String strGuess) {
+	    String[] strGuessParts = strGuess.split("");
+	    String[] strAnswerParts = strAnswer.split("");
+	    int nCows = 0;      // numbers guessed right
+	    int nBells = 0;     // numbers in exact location
+
+	    for (int nI = 0; nI < nGameCode; nI++) {
+            for (int nJ = 0; nJ < nGameCode; nJ++) {
+                if (strGuessParts[nI].equals(strAnswerParts[nJ])) {
+                    nCows++;
+                    if (nI == nJ) {
+                        nBells++;
+                    }
+                }
+            }
+        }
+        Toast.makeText(getApplicationContext(), "Cows: " + nCows + " Bells: " + nBells, Toast.LENGTH_SHORT).show();
+    }
 }
